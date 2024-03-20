72ecbcc9eca7c03fea522c0e50f8e89b30cf1fd5
sheriff-gaye <sheriffgaye5@gmail.com>
2024-03-20 03:15:55 +0000

    all

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,12 +1,20 @@
+import { Footer } from "./footer";
+import Header from "./header";
+
 type Props = {
   children: React.ReactNode;
 };
 
 const MarketingLayout = ({ children }: Props) => {
   return (
-    <div className="min-h-screen  flex flex-col ">
-      <main className="flex flex-1 flex-col items-center justify-center">{children}</main>
-    </div>
+   
+      <div className="min-h-screen  flex flex-col ">
+        <Header />
+        <main className="flex flex-1 flex-col items-center justify-center">
+          {children}
+        </main>
+        <Footer />
+      </div>
   );
 };
 
