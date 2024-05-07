c058adc972b57759c3fc1fd8d47ea698da52449d
sheriff-gaye <sheriffgaye5@gmail.com>
2024-05-07 00:36:37 +0000

    all

---
+++ scripts/seed.ts
@@ -0,0 +1,52 @@
+import "dotenv/config";
+import { drizzle } from "drizzle-orm/neon-http";
+import { neon } from "@neondatabase/serverless"
+import * as schema from "../db/schema"
+
+const sql = neon(process.env.DATABASE_URL!)
+
+
+const db = drizzle(sql, { schema });
+
+
+const main = async () => {
+    try {
+        console.log("Seeding Database");
+        await db.delete(schema.courses);
+        await db.delete(schema.userProgress);
+
+
+        await db.insert(schema.courses).values([
+            {
+                id: 1,
+                title: "Spanish",
+                imageSrc: "/es.svg"
+            },
+            {
+                id: 2,
+                title: "Italian",
+                imageSrc: "/it.svg"
+            },
+            {
+                id: 3,
+                title: "Croatian",
+                imageSrc: "/hr.svg"
+            },
+            {
+                id: 4,
+                title: "French",
+                imageSrc: "/fr.svg"
+            }
+        
+        
+        
+        ])
+
+
+    } catch (error) {
+        console.log("Failed to Seed the Database");
+
+    }
+}
+
+main();
